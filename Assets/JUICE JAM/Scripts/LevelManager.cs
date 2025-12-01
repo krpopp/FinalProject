7ed3cdb9b2375adbcca613713fc7c55c67f635ab
pennipop <kondanai@gmail.com>
2025-12-01 18:41:21 -0500

    added the level manager

---
+++ Assets/JUICE JAM/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] levelElements;
+    List<GameObject> objectsInLevel = new();
+
+    [SerializeField] int objectBuffer = 5;
+    [SerializeField] int jumpDist;
+
+    float elementSpawnPos = 0;
+    [SerializeField] Transform player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.position.x > elementSpawnPos - (objectBuffer * 10))
+        {
+            GameObject inst = Instantiate(levelElements[Random.Range(0, levelElements.Length)], new Vector2(elementSpawnPos, Random.Range(-1f, 1f)), Quaternion.identity);
+            elementSpawnPos += inst.GetComponent<Collider2D>().bounds.extents.x * 2 + jumpDist;
+            objectsInLevel.Add(inst);
+        }
+    }
+}
